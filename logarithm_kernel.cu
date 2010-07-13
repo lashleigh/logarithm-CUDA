d157d04967da3453fc9e2a7468b99bad28b8c0f1
Ashleigh Baumgardner <abaumgardner@cuda.gs.washington.edu>
2010-07-13 00:12:25 -0700

    Removed intermediate copy.

--- logarithm_kernel.cu
+++ logarithm_kernel.cu
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cuda.h>
 #include <math.h>
+#include <cuda.h>
+#include <cublas.h>
 
 __global__ void logarithm(float *device_parts, float n, int N) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -11,26 +12,24 @@ __global__ void logarithm(float *device_parts, float n, int N) {
     }
 }
 
-int num_terms = 32;
+
+int num_terms = 128;
 size_t size_terms = num_terms*sizeof(int);
 
 int main() {
-   int z = 16;
-   float *device_parts;
-   float *host_parts;
-   cudaMalloc((void**) &device_parts, size_terms);
-   cudaMallocHost((void**) &host_parts, size_terms);
-
-   logarithm <<< 2, 16, 1 >>> (device_parts, z, num_terms);
-
-   cudaMemcpy(host_parts, device_parts, size_terms, cudaMemcpyDeviceToHost);
    int i;
-   float total = 0;
-   for(i = 0; i < num_terms; i++) {
-    printf("%d %f\n", i, host_parts[i]);
-    total += host_parts[i];
-   }
-   printf("%f \n", 2*total);
+   float z = 34.7;
+   float *device_parts;
+   cudaMalloc((void**) &device_parts, size_terms);
+
+   int ThreadsPerBlock = 16;
+   int NumBlocks = (int) ((num_terms + ThreadsPerBlock - 1) / ThreadsPerBlock );
+   logarithm <<< NumBlocks, ThreadsPerBlock, 1 >>> (device_parts, z, num_terms);
+   float result = cublasSasum(num_terms, device_parts, 1);
+
+   printf("%f \n", 2*result);
+
+   cudaFree(device_parts);
 
    return 0;
 }
